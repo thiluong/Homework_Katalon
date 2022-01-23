d094e9d710cd670b7a3a9e35b4898ed4cf087ecb
ThiLuong <luong22@pnvl082.pn.com.vn>
2022-01-23 12:15:53 +0700

    Homework_HOCMAI

---
+++ Homework_HOCMAI/Object Repository/Page_Thng tin ti khon - HOCMAI/Button_KickHoat.rs
@@ -0,0 +1,110 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Button_KickHoat</name>
+   <tag></tag>
+   <elementGuidId>338195fc-5585-42f7-b49d-55b45eb00b17</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>button.btn.btn-primary1.btn-confirm</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[@type='submit']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>button</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>type</name>
+      <type>Main</type>
+      <value>submit</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>btn btn-primary1 btn-confirm</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Kích hoạt</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;addphone&quot;)/div[@class=&quot;form-group hide-step1 modal-action&quot;]/div[@class=&quot;row&quot;]/div[@class=&quot;col-sm-12 text-left&quot;]/button[@class=&quot;btn btn-primary1 btn-confirm&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//button[@type='submit']</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//form[@id='addphone']/div[2]/div/div/button</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Vui lòng nhập vào số di động của bạn'])[1]/following::button[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Kích hoạt tài khoản HOCMAI'])[1]/following::button[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Kích hoạt tài khoản thành công'])[1]/preceding::button[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Kích hoạt']/parent::*</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//button</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//button[@type = 'submit' and (text() = 'Kích hoạt' or . = 'Kích hoạt')]</value>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Homework_HOCMAI/Scripts/TC_register2/Script1642913420748.groovy
@@ -0,0 +1,43 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://hocmai.vn/loginv2/signup.php?fbclid=IwAR3VgN2ZaoB9-SHDHsIKq3u06-pO3-9ZZU1kJAKklMTT8d5uAqm_PR3pIM8')
+
+WebUI.setText(findTestObject('Page_ng k nhp hc - HOCMAI/FullName'), 'Luong Meo')
+
+WebUI.setText(findTestObject('Page_ng k nhp hc - HOCMAI/EmailAddress'), 'chuemomuoptrang123@gmail.com')
+
+WebUI.setText(findTestObject('Page_ng k nhp hc - HOCMAI/PhoneNumber'), '0372792861')
+
+WebUI.setEncryptedText(findTestObject('Page_ng k nhp hc - HOCMAI/Password'), 'BMQOnuhNWyz827lZLV1rhA==')
+
+WebUI.setEncryptedText(findTestObject('Page_ng k nhp hc - HOCMAI/RePassword'), 'BMQOnuhNWyz827lZLV1rhA==')
+
+WebUI.click(findTestObject('Page_ng k nhp hc - HOCMAI/Button_DangKy'))
+
+WebUI.click(findTestObject('Page_Thng tin ti khon - HOCMAI/Button_KickHoat'))
+
+WebUI.setText(findTestObject('Page_Thng tin ti khon - HOCMAI/Code'), '1659')
+
+WebUI.click(findTestObject('Page_Thng tin ti khon - HOCMAI/Button_KickHoat'))
+
+WebUI.closeBrowser()
+
